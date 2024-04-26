9c0b1d4ab9236a9810faece0a8ad3901e0e9d5e0
Alexander Molestov <alexander@molestov.ru>
2024-04-27 00:57:10 +0500

    Add solution

---
+++ database/database.go
@@ -0,0 +1,56 @@
+package database
+
+import (
+	"errors"
+	"log"
+	"os"
+
+	"github.com/molestov/go_final_project/models"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
+)
+
+var Db *gorm.DB
+
+func getDBPath() string {
+	const DBFile = "scheduler.db"
+	if val, exists := os.LookupEnv("TODO_DBFILE"); exists {
+		return val
+	}
+	return DBFile
+}
+
+func ConnectDB() {
+	db, err := gorm.Open(sqlite.Open(getDBPath()), &gorm.Config{
+		Logger: logger.Default.LogMode(logger.Info),
+	})
+
+	if err != nil {
+		log.Fatal("Failed to connect to database.\n", err)
+		os.Exit(1)
+	}
+
+	log.Println("connected")
+	db.Logger = logger.Default.LogMode(logger.Info)
+
+	log.Println("running migration")
+	db.AutoMigrate(&models.Task{})
+
+	Db = db
+}
+
+func ReadTasks() ([]models.Task, error) {
+	var tasks []models.Task
+	result := Db.Order("date").Limit(30).Find(&tasks)
+	return tasks, result.Error
+}
+
+func ReadTaskByID(id uint) (models.Task, error) {
+	var tasks models.Task
+	result := Db.Limit(1).Find(&tasks, models.Task{ID: id})
+	if result.RowsAffected == 0 {
+		return tasks, errors.New("No record found")
+	}
+	return tasks, result.Error
+}
